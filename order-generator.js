cf1da2cd124f6fdb06e1964d9483c6a37995d91b
edcolenopixel <98436527+edcolenopixel@users.noreply.github.com>
2022-03-10 00:59:53 -0800

    Update order-generator.js

--- order-generator.js
+++ order-generator.js
@@ -29,6 +29,7 @@ const Menu = {
 	},
 	"Jackpot Meal": {
 		price: 700,
+		emoji: '💰',
 		items: ["Money Shot", "Lucky Rings", "Copium"],
 		copypasta: '~o~ ~h~🍔JackPot Meal💰~n~ ~b~1 Money Shot, 1 Lucky Rings, 1 Copium ~w~|~g~ $400',
 	},
